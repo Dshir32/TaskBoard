4e8dde07ee7984c504d3da35c5a186fca465a47e
Shir Dahan <dshir27@gmail.com>
2021-10-10 14:34:39 +0300

    first commit
    project is done

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+// Heroku does not allow uploading just the client side, so this file is to trick Heroku for DB 
+// this will redirect to HomePage 
+header("location: taskBoardProject.html");
---
+++ taskBoardProject.js
@@ -0,0 +1,105 @@
+let allTasks = [];
+const textInputElement = document.getElementById('text');
+const dateInputElement = document.getElementById('date');
+const timeInputElement = document.getElementById('time');
+const textAlertSpan = document.getElementById('textAlert');
+const dateAlertSpan = document.getElementById('dateAlert');
+
+function clearForm(){
+    document.getElementById("tasksForm").reset();
+    textInputElement.style.backgroundColor = "white";
+    dateInputElement.style.backgroundColor = "white";
+    dateAlertSpan.innerHTML = '';
+    textAlertSpan.innerHTML = '';
+}
+
+function clearAllTasks(){
+    var tasksExist = localStorage.getItem("tasks");
+    if (tasksExist.length > 0) {
+        var deleteAllTasksConfirmation = confirm('Are you sure you want to delete ALL assignments?');
+        if(deleteAllTasksConfirmation){
+            localStorage.setItem("tasks", []);
+            allTasks = [];
+            clearForm();
+            showTasksToPage();
+        } 
+    } else {
+        alert('There are no tasks to delete!');
+    }   
+}
+
+function createTask(){
+    var text = textInputElement.value;
+    var date = dateInputElement.value;
+    var time = timeInputElement.value;
+    var taskInfo = {
+        task: text,
+        date: date,
+        time: time,
+    };
+    //Reset the alert color 
+    if (text === "") { // Validate the message box is filled
+        textAlertSpan.innerHTML = '** required';
+        textInputElement.style.backgroundColor = "pink";
+        dateInputElement.style.backgroundColor = "white";
+        dateAlertSpan.innerHTML = '';
+        return;
+        }
+    if (date === "") { // Validate the date box is filled
+        dateAlertSpan.innerHTML = '** required';
+        textAlertSpan.innerHTML = '';
+        dateInputElement.style.backgroundColor = "pink";
+        textInputElement.style.backgroundColor = "white";
+
+        return;
+        }
+    if ( new Date(date).toISOString().split('T')[0] < (new Date(Date.now())).toISOString().split('T')[0] ) { // Validate the date box is filled
+        dateAlertSpan.innerHTML = '** Invalid Date';
+        textAlertSpan.innerHTML = '';
+        dateInputElement.style.backgroundColor = "pink";
+        textInputElement.style.backgroundColor = "white";
+        return;
+        }
+    else { // Save task to local storage
+        allTasks.push(taskInfo);
+        localStorage.setItem("tasks", JSON.stringify(allTasks));
+        textAlertSpan.innerHTML = '';
+        textInputElement.style.backgroundColor = "white";
+        dateAlertSpan.innerHTML = '';
+        dateInputElement.style.backgroundColor = "white";
+    } 
+    showTasksToPage();
+    clearForm();
+}
+
+function removeTask(index){
+    var confirmation = confirm('Are you sure you want to delete this assignment?');
+    if(confirmation){
+        allTasks.splice(index, 1);
+        localStorage.setItem("tasks", JSON.stringify(allTasks));
+        $(this).parent('.taskContainer').fadeOut(200, function () {
+            $(this).remove();
+        });
+        showTasksToPage();
+    }
+}
+
+function showTasksToPage(){
+    var tasksContainer = document.getElementById("tasksContainer");
+    // Clear the page from all shown tasks
+    tasksContainer.innerHTML = "";
+    if(localStorage.getItem("tasks") !=""){
+        allTasks = JSON.parse(localStorage.getItem("tasks"));
+    for(var i = 0; i < allTasks.length ; i++) { // Loop through all tasks on local storage and HTML them to page
+            var taskDiv = `
+            <div class="taskContainer fade-in" id="taskContainer_${i}">
+                <button class="closeButton" type="button" class="btn btn-default" aria-label="Close">
+                    <span onclick="removeTask(${i})" class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                </button>
+                <p class="task">${allTasks[i].task}</p>
+                <p class="due">Due: ${allTasks[i].date} <br> ${allTasks[i].time}</p>
+            </div>`;
+            tasksContainer.innerHTML += taskDiv;
+    }
+    }  
+}
